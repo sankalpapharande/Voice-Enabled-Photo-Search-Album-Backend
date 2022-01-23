eb562d0f2629979dd3b87e7859afa3e0f572dc2b
sankalpapharande <sankalp.apharande@gmail.com>
2022-01-23 13:02:06 -0800

    Refactored: Minor code changes and comments added

--- lambda_function.py
+++ lambda_function.py
@@ -1,28 +1,25 @@
 from opensearchpy import OpenSearch, RequestsHttpConnection
-from requests_aws4auth import AWS4Auth
 import boto3
 import json
 import time
-from botocore.vendored import requests
 
-#host = 'vpc-photos-jggnnh7gdds7imimmgdtgtty7q.us-east-1.es.amazonaws.com' # For example, my-test-domain.us-east-1.es.amazonaws.com
-#host = 'search-photos2-tdwob5ezexmq2xdpkd2otj4fnm.us-east-1.es.amazonaws.com'
 host = 'search-photos-2-7rskrx3yn5dfcxg3oekayi2eei.us-east-1.es.amazonaws.com'
 region = 'us-east-1'
 service = 'es'
 headers = {"Content-Type": "application/json"}
 credentials = boto3.Session().get_credentials()
-#awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
 aws_auth = ('open-search-user', 'User@1234')
 open_search_client = OpenSearch(
-        hosts = [{'host': host, 'port': 443}],
-        http_auth = aws_auth,
-        use_ssl = True,
-        verify_certs = True,
-        connection_class = RequestsHttpConnection
-        )
+    hosts=[{'host': host, 'port': 443}],
+    http_auth=aws_auth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
 
 aws_rekognition_client = boto3.client('rekognition')
+
 
 def lambda_handler(event, context):
     print("ADDED A PRINT LINE FOR CODEPIPELINE TESTING-1")
@@ -33,40 +30,41 @@ def lambda_handler(event, context):
     for record in event['Records']:
         key = record['s3']['object']['key']
         name = record['s3']['bucket']['name']
-        
+
         print("Key:{}".format(key))
         print("name:{}".format(name))
-        
-        aws_rekognition_response = aws_rekognition_client.detect_labels(Image={'S3Object': {'Bucket': name, 'Name': key}},
-            MaxLabels=10, 
+
+        aws_rekognition_response = aws_rekognition_client.detect_labels(
+            Image={'S3Object': {'Bucket': name, 'Name': key}},
+            MaxLabels=10,
             MinConfidence=80)
-            
+
         print("Rekognition Response: {}".format(aws_rekognition_response))
-        
-        timestamp =time.time()
-        
+
+        timestamp = time.time()
+
         labels = []
         for label in aws_rekognition_response['Labels']:
             labels.append(label['Name'])
         print(labels)
-        required_json_format = {'objectKey':key,'bucket':name,'createdTimestamp':timestamp,'labels':labels}
+        required_json_format = {'objectKey': key, 'bucket': name, 'createdTimestamp': timestamp, 'labels': labels}
         print("JSON to open search:{}".format(required_json_format))
-        open_search_client.index(index="photos", 
-            doc_type="Photo", 
-            id=key, 
-            body=json.dumps(required_json_format).encode("utf-8"), 
-            refresh=True)
+        open_search_client.index(index="photos",
+                                 doc_type="Photo",
+                                 id=key,
+                                 body=json.dumps(required_json_format).encode("utf-8"),
+                                 refresh=True)
 
         count = count + 1
     print("Successfully uploaded:{}".format(count))
     return {
         'statusCode': 200,
-        'headers':{
-            'Access-Control-Allow-Origin':'*',
-            'Access-Control-Allow-Credentials':True,
-            'Access-Control-Request-Headers':'*',
-            'Access-Control-Allow-Headers':'*'
-            
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': True,
+            'Access-Control-Request-Headers': '*',
+            'Access-Control-Allow-Headers': '*'
+
         },
-        'body': json.dumps({"no_of_recrods_inserted":count})
+        'body': json.dumps({"no_of_records_inserted": count})
     }
--- search-photos.py
+++ search-photos.py
@@ -1,31 +1,30 @@
 from opensearchpy import OpenSearch, RequestsHttpConnection
-from requests_aws4auth import AWS4Auth
 import boto3
 import json
-import re
 
+# Hostname of OpenSearch Instance
 host = 'search-photos-2-7rskrx3yn5dfcxg3oekayi2eei.us-east-1.es.amazonaws.com'
 region = 'us-east-1'
 service = 'es'
 headers = {"Content-Type": "application/json"}
 credentials = boto3.Session().get_credentials()
-#awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 awsauth = ('open-search-user', 'User@1234')
 open_search_client = OpenSearch(
-        hosts = [{'host': host, 'port': 443}],
-        http_auth = awsauth,
-        use_ssl = True,
-        verify_certs = True,
-        connection_class = RequestsHttpConnection
-        )
+    hosts=[{'host': host, 'port': 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
 
 
 def get_images_from_keywords(keywords):
     query_results = []
     for label in keywords:
-        search_query_output = open_search_client.search(index="photos", body={"query":{"match":{"labels":label}}})
+        search_query_output = open_search_client.search(index="photos", body={"query": {"match": {"labels": label}}})
         query_results.append(search_query_output)
     return query_results
+
 
 def get_image_names(open_search_results):
     name_labels_dict = {}
@@ -53,11 +52,13 @@ SINGULAR_SUFFIX = [
     ('s', '')
 ]
 
+# Making Plural words sigular
 def singularize(noun):
     for suffix, singular_suffix in SINGULAR_SUFFIX:
         if noun.endswith(suffix):
             return noun[:-len(suffix)] + singular_suffix
     return noun
+
 
 def get_open_search_supported_keywords(keywords):
     modified_keywords = []
@@ -65,77 +66,65 @@ def get_open_search_supported_keywords(keywords):
         if keyword is not None:
             modified_keywords.append(singularize(keyword).capitalize())
     return modified_keywords
-    
+
+
 def lambda_handler(event, context):
     try:
         print("event:{}".format(event))
         print("context:{}".format(context))
+
         # Lex will give keywords
         print("creating lex client")
         lex_bot_client = boto3.client('lexv2-runtime')
         print("lex client created")
         print(lex_bot_client)
-        
-        #user_input = "get me pics of dogs and cats"
-        # user_input = "Show me pics of dogs and cats"
-    
-        
         user_input = event["queryStringParameters"]["q"]
-        #user_input = "get me pics of Cat"
-        
         print("User Input:{}".format(user_input))
-        
-        
-        
+
         response = lex_bot_client.recognize_text(
-                botAliasId="TSTALIASID",
-                botId="1RBXMHAUMG",
-                localeId= "en_US",
-                text = user_input,
-                sessionId= "877755470769942"
-            )
-        
-        print("*******************Lex bot response:{}".format(response))
-        
+            botAliasId="TSTALIASID",
+            botId="1RBXMHAUMG",
+            localeId="en_US",
+            text=user_input,
+            sessionId="877755470769942"
+        )
+
+        print("Lex bot response:{}".format(response))
+
         keywords = []
         q1_value = response["sessionState"]["intent"]["slots"]["q1"]
         q2_value = response["sessionState"]["intent"]["slots"]["q2"]
         print(response["sessionState"]["intent"]["slots"])
         if q1_value is not None:
             keywords.append(q1_value["value"]["interpretedValue"])
-        
+
         if q2_value is not None:
             keywords.append(q2_value["value"]["interpretedValue"])
-        
-        
+
         modified_keywords = get_open_search_supported_keywords(keywords)
-        
+
         print("Keywords are:{}".format(keywords))
         print("Modified keywords are:{}".format(modified_keywords))
         open_search_results = get_images_from_keywords(modified_keywords)
         print(open_search_results)
-        
+
         image_names_labels = get_image_names(open_search_results)
-        base_url = "https://yuktib2.s3.amazonaws.com/"
+        base_url = "https://<s3 bucket where UI is hosted>.s3.amazonaws.com/"
         results = []
         for image in image_names_labels.keys():
-            image_info_json = {}
-            image_info_json["url"] = base_url+image
-            image_info_json["labels"] = image_names_labels[image]
+            image_info_json = {"url": base_url + image, "labels": image_names_labels[image]}
             results.append(image_info_json)
-        
+
         print("The result images are:{}".format(results))
         return {
-        'statusCode': 200,
-        'headers':{
-            'Access-Control-Allow-Origin':'*',
-            'Access-Control-Allow-Credentials':True
-            # 'Access-Control-Request-Headers':'*',
-            # 'Access-Control-Allow-Headers':'*'
-        },
-        'body': json.dumps({"results":results})
-    }
-        
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': True
+            },
+            'body': json.dumps({"results": results})
+        }
+
     except Exception as e:
         return {
             'code': 0,
